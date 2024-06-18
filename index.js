8bd87d17c0dcdd49947994549671808171790b77
Samantha Sun <samisun1047@gmail.com>
2024-06-17 21:36:02 -0700

    add code for server setup

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 import express from "express";
-import noteRoutes from "./routes/noteRoutes.js";
+import videoRoutes from "./routes/videos.js";
 import cors from "cors";
+import fs from "fs";
+import "dotenv/config";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("This is a homePage for BrainFlix videos API, please make a request!");
+});
+
+app.use("/videos", videoRoutes);
+
+app.listen(PORT, () => {
+  console.log("App is running on port ", PORT);
+});
--- routes/videos.js
+++ routes/videos.js
@@ -1,3 +1,7 @@
 import express from "express";
 import fs from "fs";
 import crypto from "crypto";
+
+const router = express.Router();
+
+export default router;
