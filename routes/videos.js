8f5a74352a8dc807ed9cf58e6852f2b4a2ea17c1
Samantha Sun <samisun1047@gmail.com>
2024-06-17 22:32:57 -0700

    add code for get endpoints

--- routes/videos.js
+++ routes/videos.js
@@ -4,4 +4,30 @@ import crypto from "crypto";
 
 const router = express.Router();
 
+function readData() {
+  const videosData = fs.readFileSync("./data/videos.json");
+  const parsedData = JSON.parse(videosData);
+  return parsedData;
+}
+
+router.get("/", (req, res) => {
+  const videos = readData();
+  res.json(videos);
+});
+
+router.get("/:id", (req, res) => {
+  const videos = readData();
+
+  const singlevideo = videos.find((video) => video.id === req.params.id);
+  if (singlevideo) {
+    res.json(singlevideo);
+  } else {
+    // If no note found with the given ID, return 404 error
+    res.status(404).json({
+      message: "Please enter a valid ID",
+      error: "404",
+    });
+  }
+});
+
 export default router;
